afaa2d4505a3c4e75962c8e62daab6acac8e214b
manjuhubballi02@gmail.com <manjuhubballi02@gmail.com>
2019-12-25 21:35:14 +0530

    curd operation

---
+++ Nodejs/controller/admin/designation.js
@@ -0,0 +1,48 @@
+const { designation, validateDesignation } = require('../../models/admin/designation');
+const _=require('lodash')
+exports.designationGetAll = async (req, res) => {
+        const designations = await designation.find();
+        res.send(designations);
+      }
+
+exports.designationPost = async (req, res) => {
+    const { error } = validateDesignation(req.body);
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let designations = new designation(_.pick(req.body,['designation','status']))
+    postDesignation = await designations.save().then().catch(function (err) {
+             res.status(400).send(err.message);  
+        });
+   return res.send(postDesignation);
+}
+
+exports.designationUpdate = async (req, res) => {
+    const { error } = validateDesignation(req.body);
+
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const designations = await designation.findByIdAndUpdate(req.params.id, {
+        designation:req.body.designation,
+        status:req.body.status,
+
+    },
+    { runValidators: true, context: 'query' },
+    function (err) {
+       
+    },
+   
+        {
+            new: true
+        }, 
+       
+       ).then().catch(function (err) {
+         res.status(400).send(err.message); 
+    });
+    res.send(designations);
+}
+
+exports.designationDelete = async (req, res) => {
+    const designations = await designation.findByIdAndRemove(req.params.id);
+    res.send(designations);
+}     
